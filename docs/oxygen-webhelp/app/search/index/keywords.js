a493eaf5225176107b6e09d96570a29e2c509358
Zhijun Gao <galty687@gmail.com>
2024-11-12 16:46:32 +0800

    add dita source and published docs

---
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,15 @@
+var keywords=[{w:"\u4F7F\u7528HDMI\u7EBF\u8FDB\u884C\u6295\u5C4F",p:["p0"]},{w:"Context",p:["p1"]},{w:"Sensitive",p:["p1"]},{w:"Help",p:["p1"]},{w:"Airplay\u6295\u5C4F\u6B65\u9AA4",p:["p2"]},{w:"\u5982\u4F55\u4F7F\u7528HDMI\u6295\u5C4F",p:["p3"]},{w:"\u5982\u4F55\u4F7F\u7528Airplay\u5C06iPad\u6295\u5C4F\u81F3\u6295\u5F71\u4EEA",p:["p4"]},{w:"AirPlay\u53C2\u6570\u4FE1\u606F",p:["p5"]},{w:"\u4E0D\u80FD\u4F7F\u7528AirPlay\u6295\u5C4F",p:["p6"]},{w:"Airplay",p:["p7"]},{w:"\u4EC0\u4E48\u662FHDMI",p:["p8"]}];
+var ph={};
+ph["p0"]=[0];
+ph["p1"]=[1, 2, 3];
+ph["p2"]=[4];
+ph["p3"]=[5];
+ph["p4"]=[6];
+ph["p5"]=[7];
+ph["p6"]=[8];
+ph["p7"]=[9];
+ph["p8"]=[10];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {6:2,5:2,4:2,2:-1,8:3,7:2,3:-1,0:3};
